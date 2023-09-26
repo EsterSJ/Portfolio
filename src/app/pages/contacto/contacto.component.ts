958425386e357de6805a0744ec4b0e109b920669
EsterSJ <ester_s@hotmail.com>
2023-09-26 10:57:29 +0200

    contacto responsive

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contacto',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class ContactoComponent {
 
+  public enviar() {
+    setTimeout(() => {
+      Swal.fire(
+        '¡Gracias!',
+        'Mensaje enviado correctamente',
+        'success'
+      );
+    }, 500); 
+  }
+  
 }
